5f50c8cd150194f7a31e6361fb170497b30b5a41
EthanBarker <EthanJBarker@outlook.com>
2023-03-15 10:57:20 +0000

    Ex10 for OTA working with my own code

--- My Code/Ex10/Ex10.ino
+++ My Code/Ex10/Ex10.ino
@@ -1,96 +1,71 @@
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
+#include <HTTPClient.h>
 #include <Update.h>
 
 const char* ssid = "FLAT-6-2G";
 const char* password = "ptpassword";
+const char* FIRMWARE_SERVER_IP_ADDR = "143.167.81.144"; // Change this to your server's IP address
+const int FIRMWARE_SERVER_PORT = 8000; // Change the port if needed
 
-const char* server_ip = "192.168.1.100"; // IP address of your local HTTP server
-const int server_port = 8000; // Port of your local HTTP server
-const String firmware_url = "/firmware.bin"; // URL of the firmware binary file
-
-int firmwareVersion = 1; // current firmware version
-
-WebServer server(80);
+int firmwareVersion = 2; // Increment this value and recompile the sketch for each new version
 
 void setup() {
   Serial.begin(115200);
-
   WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  server.on("/update", HTTP_POST, [](){
-    server.send(200, "text/plain", "OTA update started.");
-    delay(1000);
-
-    WiFiClient client;
-    if (client.connect(server_ip, server_port)) {
-      Serial.println("Connected to server, downloading firmware...");
-      client.print(String("GET ") + firmware_url + " HTTP/1.1\r\n" +
-                   "Host: " + server_ip + "\r\n" +
-                   "Connection: close\r\n\r\n");
-
-      while(client.connected()){
-        if(client.available()){
-          String line = client.readStringUntil('\n');
-          if(line == "\r"){
-            Serial.println("Headers received, downloading firmware binary...");
-            break;
-          }
-        }
-      }
-
-      if (Update.begin()) {
-        while(client.connected()){
-          uint8_t buff[128] = { 0 };
-          int len = client.readBytes(buff, 128);
-          if(len > 0){
-            Update.write(buff, len);
-          }
-          else{
-            break;
-          }
-        }
-
-        if (Update.end()) {
-          Serial.println("Firmware updated successfully.");
-          firmwareVersion++; // increment firmware version
-          // update firmwareVersion in the .ino file
-          String ino_file_content = "";
-          File ino_file = SPIFFS.open("/main.ino", "r");
-          while (ino_file.available()) {
-            ino_file_content += (char)ino_file.read();
-          }
-          ino_file.close();
-          ino_file_content.replace("int firmwareVersion = " + String(firmwareVersion - 1), "int firmwareVersion = " + String(firmwareVersion));
-          ino_file = SPIFFS.open("/main.ino", "w");
-          ino_file.print(ino_file_content);
-          ino_file.close();
-        }
-        else {
-          Serial.println("Error updating firmware.");
-        }
-      }
-      else {
-        Serial.println("Could not begin OTA update.");
-      }
-    }
-    else {
-      Serial.println("Could not connect to server.");
-    }
-  });
-
-  server.begin();
+  Serial.println("\nConnected to Wi-Fi");
+  performOTAUpdate();
 }
 
 void loop() {
-  server.handleClient();
+  // Your main code here
+}
+
+void performOTAUpdate() {
+  HTTPClient http;
+  char url[256];
+
+  snprintf(url, sizeof(url), "http://%s:%d/version.txt", FIRMWARE_SERVER_IP_ADDR, FIRMWARE_SERVER_PORT);
+  http.begin(url);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    int latestVersion = atoi(http.getString().c_str());
+    if (latestVersion > firmwareVersion) {
+      snprintf(url, sizeof(url), "http://%s:%d/firmware_%d.bin", FIRMWARE_SERVER_IP_ADDR, FIRMWARE_SERVER_PORT, latestVersion);
+      http.begin(url);
+      int httpCode = http.GET();
+
+      if (httpCode == 200) {
+        Serial.println("Starting OTA update...");
+        if (Update.begin(http.getSize())) {
+          size_t written = Update.writeStream(*http.getStreamPtr());
+          if (written == http.getSize()) {
+            Serial.println("Written : " + String(written) + " successfully");
+          } else {
+            Serial.println("Written only : " + String(written) + "/" + String(http.getSize()) + ". Retry?" );
+          }
+          if (Update.end()) {
+            Serial.println("OTA update complete. Rebooting...");
+            ESP.restart();
+          } else {
+            Serial.println("Error Occurred. Error #: " + String(Update.getError()));
+          }
+        } else {
+          Serial.println("Not enough space to begin OTA");
+        }
+      } else {
+        Serial.printf("Unable to fetch firmware: HTTP code %d\n", httpCode);
+      }
+    } else {
+      Serial.println("Firmware is up to date");
+    }
+  } else {
+    Serial.printf("Unable to fetch version.txt: HTTP code %d\n", httpCode);
+  }
+  http.end();
 }
