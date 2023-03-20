1a9207c32db568537354157e7cc515eac4ef91a3
EthanBarker <EthanJBarker@outlook.com>
2023-03-20 15:40:22 +0000

    Updated Ex9 for use in LA1

--- My Code/Ex9/Ex9.ino
+++ My Code/Ex9/Ex9.ino
@@ -1,10 +1,10 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-const char* ssid = "Ethan's very cool ESP32"; // Replace with your SSID
-const char* password = "Password123"; // Replace with your password
+const char* ssid = "ESP32 Access Point"; // SSID of the ESP32 access point
+const char* password = "password"; // Password for the access point
 
-WebServer webServer(80); // Create a web server on port 80
+WebServer server(80); // Create a web server on port 80
 
 void handleRoot() {
   String page = "<html><body><h1>Available Wi-Fi Networks</h1><ul>";
@@ -12,61 +12,72 @@ void handleRoot() {
   for (int i = 0; i < n; i++) {
     page += "<li>";
     page += WiFi.SSID(i);
+    if (WiFi.encryptionType(i) != WIFI_AUTH_OPEN) {
+      page += " (password protected)";
+    }
     page += "  <a href=\"/connect?ssid=";
     page += WiFi.SSID(i);
     page += "\">Connect</a>";
     page += "</li>";
   }
   page += "</ul></body></html>";
-  webServer.send(200, "text/html", page);
+  server.send(200, "text/html", page);
 }
 
 void handleConnect() {
-  String ssid = webServer.arg("ssid");
-  String key = webServer.arg("key");
+  String ssid = server.arg("ssid");
   Serial.print("Connecting to ");
   Serial.println(ssid);
+
+  String key = "";
+  if (server.hasArg("key")) {
+    key = server.arg("key");
+  } else {
+    String page = "<html><body><h1>Enter Wi-Fi Password</h1>";
+    page += "<form method='get' action='/connect'>";
+    page += "<label for='key'>Password:</label>";
+    page += "<input type='password' id='key' name='key'><br><br>";
+    page += "<input type='submit' value='Connect'>";
+    page += "<input type='hidden' name='ssid' value='" + ssid + "'>";
+    page += "</form></body></html>";
+    server.send(200, "text/html", page);
+    return;
+  }
+
   WiFi.begin(ssid.c_str(), key.c_str());
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting...");
   }
   Serial.println("Connected");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  webServer.send(200, "text/html", "<html><body><h1>Connected to " + ssid + "</h1><p><a href='/'>Go back to Home Page</a></p></body></html>");
-  WiFi.printDiag(Serial);
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  server.send(200, "text/html", "<html><body><h1>Connected to " + ssid + "</h1><p><a href='/'>Go back to Home Page</a></p></body></html>");
 }
 
 void handleNotFound() {
-  webServer.send(404, "text/plain", "404: Not found");
+  server.send(404, "text/plain", "404: Not found");
 }
 
 void setup() {
-  Serial.begin(115200); // Start serial communication
-
-  // Connect to WiFi access point
+  Serial.begin(115200);
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, password);
-
-  Serial.println("WiFi access point started");
+  Serial.println("Access Point started");
   Serial.print("SSID: ");
   Serial.println(ssid);
   Serial.print("Password: ");
   Serial.println(password);
-
-  // Register web page handlers
-  webServer.on("/", handleRoot);
-  webServer.on("/connect", handleConnect);
-  webServer.onNotFound(handleNotFound);
-
-  // Start the server
-  webServer.begin();
-
+  server.on("/", handleRoot);
+  server.on("/connect", handleConnect);
+  server.onNotFound(handleNotFound);
+  server.begin();
   Serial.println("Web server started");
 }
 
 void loop() {
-  // Handle incoming client requests
-  webServer.handleClient();
+  server.handleClient();
 }
+ 
